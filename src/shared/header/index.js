436e963118c33e4855f0a57a1d82c2eb75d8a6d8
Daniel Gómez <daniel.gomez@samsistemas.com.ar>
2017-06-16 15:10:29 -0300

    Add links in header

--- src/shared/header/index.js
+++ src/shared/header/index.js
@@ -1,9 +1,19 @@
 import xs from 'xstream';
-import { h1 } from '@cycle/dom';
+import { h1, div, a } from '@cycle/dom';
 
 function Header(sources) {
 
-    const vtree$ = xs.of(h1( "This is a title"));
+    const vtree$ = xs.of(
+        div([
+            h1( "This is a title"),
+            a(".link_home", {attrs: {href: "#home"}}, [
+                "Home"
+            ]),
+            a(".link_about", {attrs: {href: "#about"}}, [
+                "About"
+            ])
+        ])
+    );
 
     return {
         DOM: vtree$
